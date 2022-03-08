1e519bfcf6f5237b4c8dc0ef5a05ff32c9aa45dc
waibazen <waibazen@gmail.com>
2022-03-07 21:49:27 -0800

    first commit all files

---
+++ src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
@@ -0,0 +1,19 @@
+package com.example.EmployeeManagement;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
