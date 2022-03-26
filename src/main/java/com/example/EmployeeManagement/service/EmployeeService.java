413decb16a395654a1ef4cf2fe0cf56f0e926b94
waibazen <waibazen@gmail.com>
2022-03-25 21:44:39 -0700

    just added a string

--- src/main/java/com/example/EmployeeManagement/service/EmployeeService.java
+++ src/main/java/com/example/EmployeeManagement/service/EmployeeService.java
@@ -37,7 +37,7 @@ public class EmployeeService {
 
     public Employee findEmployeeById(Long id) {
         return employeeRepo.findEmployeeById(id)
-                .orElseThrow(() -> new UserNotFoundException("User by id : " + id + " was not found"));
+                .orElseThrow(() -> new UserNotFoundException("User by id is : " + id + " was not found"));
     }
 
     public void deleteEmployee(Long id){
